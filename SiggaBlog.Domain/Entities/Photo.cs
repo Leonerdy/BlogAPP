4f2356f3db3b2f27f1614a40257d6b94d1d8697f
Anselmo Silva <enge.leon@gmail.com>
2025-05-20 20:02:31 -0300

    revertendo erros

--- SiggaBlog.Domain/Entities/Photo.cs
+++ SiggaBlog.Domain/Entities/Photo.cs
@@ -6,6 +6,6 @@ namespace SiggaBlog.Domain.Entities
         public int AlbumId { get; set; }
         public string Title { get; set; }
         public string Url { get; set; }
-        public string ThumbnailUrll { get; set; }
+        public string ThumbnailUrl { get; set; }
     }
 } 
